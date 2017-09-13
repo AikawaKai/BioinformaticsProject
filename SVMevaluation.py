2e9be42a2925d5663766c001a3ee96e0188ae123
Marco <marco.odore@gmail.com>
2017-09-13 03:03:17 +0200

    added SVM training

---
+++ SVMevaluation.py
@@ -0,0 +1,25 @@
+from utility.loadDataSet import loadDataSet
+from utility.loadDataSet import loadClasses
+from utility.loadDataSet import transpose
+from sklearn import svm
+from sklearn.multiclass import OneVsRestClassifier
+import sys
+from numpy import array
+
+if __name__ == '__main__':
+    print("[INFO] Loading dataset.")
+    dataset_dir = sys.argv[1]
+    annotations = sys.argv[2]
+    (features, X) = loadDataSet(dataset_dir)
+    print(features)
+    print(X)
+    print("[INFO] Dataset loaded (Features, X).")
+    print("[INFO] Loading classes.")
+    (classes, Y) = loadClasses(annotations)
+    print(len(classes))
+    print(Y)
+    print("[INFO] Classes loaded (Y).")
+    print(len(Y[0]), len(features))
+    print("[INFO] SVM Training Started.")
+    clf = svm.SVC(class_weight="balanced")
+    OneVsRestClassifier(clf).fit(X, Y).predict(X)
