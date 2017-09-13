a50611f2fea748966b097b47db00d5e9aa58463b
Marco <marco.odore@gmail.com>
2017-09-13 19:13:44 +0200

    updated

--- SVMevaluation.py
+++ SVMevaluation.py
@@ -4,12 +4,14 @@ from utility.loadDataSet import transpose
 from sklearn import svm
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import precision_recall_curve
 from utility.scorer import *
 from imblearn.over_sampling import ADASYN
 from imblearn.combine import SMOTEENN
 import csv
 import sys
 from numpy import array
+from collections import Counter
 
 if __name__ == '__main__':
     print("[INFO] Loading dataset.")
@@ -29,14 +31,20 @@ if __name__ == '__main__':
     kf = StratifiedKFold(n_splits=5)
     clf = svm.SVC(class_weight="balanced")
     res = [""]
+    counter_confusion_matrix = [[] for i in range(len(features))]
     for y in Y:
         auc = 0
         for train_index, test_index in kf.split(X, y):
             X_train, X_test = X[train_index], X[test_index]
             y_train, y_test = y[train_index], y[test_index]
             clf.fit(X_train, y_train)
-            auc_i = my_scorer(clf, X_test, y_test)
+            auc_i, diff_ = my_scorer(clf, X_test, y_test)
             auc+=auc_i
+            i=0
+            for index in test_index:
+                counter_confusion_matrix[index].append(diff_[i])
+                i+=1
+            # print(counter_confusion_matrix)
         cur_auc = auc/5
         res.append(cur_auc)
         print(cur_auc)
@@ -46,3 +54,18 @@ if __name__ == '__main__':
         csv_writer = csv.writer(f_i, delimiter=",")
         csv_writer.writerow(["AUC"]+list(classes))
         csv_writer.writerow(res)
+
+    with open("./results/SVM_Precision_Recall_multilabel_results.csv") as f_i:
+        csv_writer = csv.writer(f_i, delimiter=",")
+        csv_writer.writerow(["Precision", "Recall"])
+        precision = 0
+        recall = 0
+        for inst in counter_confusion_matrix:
+            dict_ = Counter(inst)
+            tp = dict_["TP"]
+            tn = dict_["TN"]
+            fn = dict_["FN"]
+            fp = dict_["FP"]
+            precision+=tp/(tp+fp)
+            recall+=tp/(tp+fn)
+        csv_writer.writerow([precision/len(counter_confusion_matrix), recall/len(counter_confusion_matrix)])
--- utility/scorer.py
+++ utility/scorer.py
@@ -1,17 +1,39 @@
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import auc, roc_curve, classification_report
 from sklearn.metrics import accuracy_score, precision_score, recall_score
+from collections import Counter
 auc_res = []
+
+def checkPredict(c):
+    res = ''
+    if c[0]==c[1]:
+        res+='T'
+        if c[0] == 0:
+            res+='N'
+            return res
+        res+='P'
+        return res
+    res+='F'
+    if c[0] == 0:
+        res+='P'
+        return res
+    return res+'N'
 
 def getScores(estimator, x, y):
     global auc_res
     yPred = estimator.predict(x)
-    fpr, tpr, thresholds = roc_curve(y, yPred, pos_label=1)
+    yScores = estimator.decision_function(x)
+    # print(yScores)
+    fpr, tpr, thresholds = roc_curve(y, yScores, pos_label=1)
+    # print(thresholds)
     # print(classification_report(y, yPred))
     print(confusion_matrix(y, yPred))
-    auc_res.append(auc(fpr, tpr))
-    return (precision_score(y, yPred, average='binary'), auc(fpr, tpr))
+    auc_ = auc(fpr, tpr)
+    auc_res.append(auc_)
+    return (precision_score(y, yPred, average='binary'), auc_, yPred)
 
 def my_scorer(estimator, x, y):
-    p, roc_area = getScores(estimator, x, y)
-    return roc_area
+    p, roc_area, yPred = getScores(estimator, x, y)
+    diffs_ = [checkPredict(c) for c in zip(y, yPred)]
+    print(Counter(diffs_))
+    return (roc_area, diffs_)
